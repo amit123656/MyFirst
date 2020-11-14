06fc7b4b0b67eece8d2c0786ae94870f34f40220
Nirbhay Gandhi <73874714+nanonirbhay@users.noreply.github.com>
2020-11-14 17:31:08 +0530

    basic +,-,*,/

--- Calc.java
+++ Calc.java
@@ -5,7 +5,7 @@ public class MyCalc{
 public static void main(String[] args){
 Scanner in=new Scanner(System.in);
 System.out.println("Wlecome to Calculator");
-System.out.println("1. +\n2. -\nEnter the choise: ");
+System.out.println("1. +\n2. -\3. *\n4. /\nEnter the choise: ");
 int ch=in.nextInt();
 switch (ch){
 case 1:
@@ -13,6 +13,12 @@ add();
 break;
 case 2:
 sub();
+break;
+case 3:
+mul();
+break;
+case 4:
+div();
 break;
 default:
 System.out.println("Invalid response!"); }
@@ -32,7 +38,21 @@ System.out.println("n1: ");
 int n1=in.nextInt();
 System.out.println("n2: ");
 int n2=in.nextInt();
-return (n1-n2);
-}
-
+return (n1-n2);}
+  
+public static double mul(){
+Scanner in=new Scanner(System.in);
+System.out.println("n1: ");
+int n1=in.nextInt();
+System.out.println("n2: ");
+int n2=in.nextInt();
+return (n1*n2);}
+  
+public static double div(){
+Scanner in=new Scanner(System.in);
+System.out.println("n1: ");
+int n1=in.nextInt();
+System.out.println("n2: ");
+int n2=in.nextInt();
+return (n1/n2);}
 }
