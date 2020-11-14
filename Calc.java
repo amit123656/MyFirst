2af612b69bbc7edc12f454ffa6f66a47e8a8d92d
Nirbhay Gandhi <73874714+nanonirbhay@users.noreply.github.com>
2020-11-14 17:47:54 +0530

    basic+adv{facto}

--- Calc.java
+++ Calc.java
@@ -5,7 +5,7 @@ public class MyCalc{
 public static void main(String[] args){
 Scanner in=new Scanner(System.in);
 System.out.println("Wlecome to Calculator");
-System.out.println("1. +\n2. -\3. *\n4. /\nEnter the choise: ");
+System.out.println("1. +\n2. -\3. *\n4. /\n5. factorial\nEnter the choise: ");
 int ch=in.nextInt();
 switch (ch){
 case 1:
@@ -30,7 +30,7 @@ System.out.println("n1: ");
 int n1=in.nextInt();
 System.out.println("n2: ");
 int n2=in.nextInt();
-return (n1+n2);}
+System.out.prinln(n1+n2);}
 
 public static double sub(){
 Scanner in=new Scanner(System.in);
@@ -38,7 +38,7 @@ System.out.println("n1: ");
 int n1=in.nextInt();
 System.out.println("n2: ");
 int n2=in.nextInt();
-return (n1-n2);}
+System.out.prinln(n1-n2);}
   
 public static double mul(){
 Scanner in=new Scanner(System.in);
@@ -46,7 +46,7 @@ System.out.println("n1: ");
 int n1=in.nextInt();
 System.out.println("n2: ");
 int n2=in.nextInt();
-return (n1*n2);}
+System.out.prinln(n1*n2);}
   
 public static double div(){
 Scanner in=new Scanner(System.in);
@@ -54,5 +54,14 @@ System.out.println("n1: ");
 int n1=in.nextInt();
 System.out.println("n2: ");
 int n2=in.nextInt();
-return (n1/n2);}
+System.out.prinln(n1/(double)n2);}
+  
+public static int facto(){
+Scanner in=new Scanner(System.in);
+System.out.println("n: ");
+int n=in.nextInt();
+int fact=1, i;
+for(i=1;i<=n;i++){
+fact=fact*i;}
+System.out.prinln(n+"!: "+fact);}
 }
